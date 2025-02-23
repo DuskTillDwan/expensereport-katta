a76736c5c28b87ee981892bcb42972e7e3eedc2f
Taylor Williams <dwan.williams.t@gmail.com>
2025-02-22 18:08:03 -0600

    ! r added bigReportTest. added createExpense method for test.

--- expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
+++ expensereport-java/src/test/java/com/nelkinda/training/ExpenseReportTest.java
@@ -7,6 +7,7 @@ import org.approvaltests.ApprovalUtilities;
 import java.io.ByteArrayOutputStream;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 
 class ExpenseReportTest {
@@ -17,5 +18,34 @@ class ExpenseReportTest {
         report.printReport(Collections.emptyList(), new Date(0));
         Approvals.verify(output);
     }
+    @Test
+    void bigReport(){
+        ByteArrayOutputStream output = new ApprovalUtilities().writeSystemOutToStringBuffer();
+        ExpenseReport report = new ExpenseReport();
+        List<Expense> expenses = List.of(
+                createExpense(ExpenseType.DINNER, 5000),
+                createExpense(ExpenseType.DINNER, 5001),
+                createExpense(ExpenseType.BREAKFAST, 5000),
+                createExpense(ExpenseType.BREAKFAST, 5001),
+                createExpense(ExpenseType.CAR_RENTAL, 50),
+                createExpense(ExpenseType.CAR_RENTAL, Integer.MAX_VALUE)
+        );
+        report.printReport(Collections.emptyList(), new Date(0));
+        Approvals.verify(output);
+    }
 
+    private static Expense createExpense(ExpenseType expenseType, int amount) {
+        Expense expense = new Expense();
+        expense.type = expenseType;
+        expense.amount = amount;
+        return expense;
+    }
+/**
+ * DINNER 5000,
+ * DINNER 5001,
+ * BREAKFAST 1000,
+ * BREAKFAST 1001,
+ * CAR_RENTAL 60
+ * CAR_RENTAL Integer.MAX_VALUE, as there is no limit
+ */
 }
